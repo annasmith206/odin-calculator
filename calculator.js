d33927a813cab97175f3ca64e9c8e40b5b6ef010
Anna <aksmithnw@gmail.com>
2024-02-22 15:13:47 -0800

    Add onClick event listeners to every button

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+initializeButtonEvents();
+
+function initializeButtonEvents(){
+    const buttons = Array.from(document.querySelectorAll("button"));
+    for (let button of buttons) {
+        button.addEventListener("click", onClick);
+    }
+}
+
 function operate(num1, num2, operator) {
   switch (operator) {
     case "+":
@@ -9,4 +18,8 @@ function operate(num1, num2, operator) {
     case "/":
       return num1 / num2;
   }
+}
+
+function onClick(event) {
+    alert(`clicked ${event.target.textContent}`)
 }
